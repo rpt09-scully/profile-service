8ceefc7f5464bdab25b4a359129e828b557fa337
Matt Weber <matt@weberdesign-ak.com>
2018-11-15 18:16:46 -0900

    Setting up db, not working currently

---
+++ database-mysql/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  user: 'YOUR_USERNAME_HERE', 
+  // if you are using the pairing stations, the user is `student`
+  password: 'YOUR_PASSWORD_HERE', 
+  // if you are using the paring stations, the password is `student`
+  // if you do not have a password, delete this key/value pair from
+  // the object
+  database: 'minteger'
+};
