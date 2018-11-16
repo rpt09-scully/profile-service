67afc8399cefd120c5bb1b01783d2e04b7dba564
Matt Weber <matt@weberdesign-ak.com>
2018-11-15 16:52:12 -0900

    Install express, server live on port 3000

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database-mysql');
 
 const app = express();
 const PORT = 3000;
@@ -8,7 +7,7 @@ const PORT = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 
 
