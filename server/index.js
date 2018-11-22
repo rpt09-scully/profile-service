1337d7b5c7f64bfb7af2207b1d229588a84d4462
Matt Weber <matt@weberdesign-ak.com>
2018-11-22 11:48:36 -0900

    user endpoint serves real data, still need to include fav activities

--- server/index.js
+++ server/index.js
@@ -14,8 +14,29 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/user/:id', function (req, res) {
   db.getUser(req.params.id)
+  .then((rawUserData) => {
+    const rawData = rawUserData[0];
+    let userData = {
+      "data": {
+        "type": "users",
+        "id": rawData.prof_id,
+        "attributes": {
+          "first_name": rawData.first_name,
+          "last_name": rawData.last_name,
+          "email": rawData.email,
+          "location": rawData.location,
+          "date_joined": rawData.date_joined.toJSON().substring(0,10),
+          "favorite_activities": ["Hunting", "Hiking", "Fishing"], // Need to pull real data
+          "bio": rawData.bio,
+          "photo_url": rawData.photo_url,
+          "pro": !!+rawData.pro
+        }
+      }
+    }
+
+    return userData;
+  })
   .then((userData) => {
-    // NEED TO FORMAT THIS DATA FIRST
     res.send(userData);
   });
 });
