d370176d8d5473e79c483ace268a3ed6d537e8ba
Matt Weber <matt@weberdesign-ak.com>
2018-11-24 13:04:43 -0900

    Started outline for profile, not rendering updates though, still hellow world

--- client/src/components/profile.jsx
+++ client/src/components/profile.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+// MODEL https://www.alltrails.com/members/rpeh
 
 export default class App extends Component {
+    constructor() {
+    super();
+
+    this.state = {
+        id: 22,
+        first_name: 'Joel',
+        last_name: 'Vandervort',
+        full_name: first_name + last_name,
+        email: 'tbecker@example.net',
+        location: 'Fletachester, VA',
+        date_joined: '1979-02-20',
+        favorite_activities: ['Hiking', 'Hunting', 'Fishing'],
+        bio: 'Aut sequi dolore temporibus quia qui consequatur. Possimus non fugiat perspiciatis voluptas. Enim aut quia minus ipsam. Dolore corrupti et tenetur sit.',
+        photo_url: 'http://lorempixel.com/640/480/',
+        pro: true
+    };
+  }
     render() {
         return (
-        <div>
-            Hello World
+        <div id="profile-container">
+            <div id="profile-topbar">
+                <h3>Profile</h3>
+            </div>
+            <div id="profile-main">
+                <div id="profile-info">
+                    <div id="profile-image-container">
+                        <img src={this.state.photo_url} alt={this.state.full_name}/>
+                    </div>
+                </div>
+            </div>
         </div>
         )
     }
