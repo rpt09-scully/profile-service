766c819923e833c83acff90b0d5517ad288e4d5b
Matt Weber <matt@weberdesign-ak.com>
2018-12-15 08:48:41 -0900

    Added cors to server index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('../database');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3002;
