2d81c1a597ce6bffc6df18c983aea23d32689610
Matt Weber <matt@weberdesign-ak.com>
2018-12-15 12:25:35 -0900

    Update to work on id entered

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -6,63 +6,87 @@ import Pro from './Pro.jsx';
 
 export default class Profile extends Component {
     constructor() {
-    super();
+        super();
 
-    this.state = {
-        id: 22,
-        first_name: 'Joel',
-        last_name: 'Vandervort',
-        full_name: 'Joel Vandervort',
-        email: 'tbecker@example.net',
-        location: 'Fletachester, VA',
-        date_joined: '1979-02-20',
-        favorite_activities: ['Hiking', 'Hunting', 'Fishing'],
-        bio: 'Aut sequi dolore temporibus quia qui consequatur. Possimus non fugiat perspiciatis voluptas. Enim aut quia minus ipsam. Dolore corrupti et tenetur sit.',
-        photo_url: 'http://lorempixel.com/100/100/',
-        pro: false
-        // Could still add Reputation and followers
-    };
+        this.state = {
+            id: '',
+            first_name: '',
+            last_name: '',
+            full_name: '',
+            email: '',
+            location: '',
+            date_joined: '',
+            favorite_activities: [],
+            bio: '',
+            photo_url: '',
+            pro: false
+            // Could still add Reputation and followers
+        };
 
-    this.convertDate = this.convertDate.bind(this);
-  }
+        this.convertDate = this.convertDate.bind(this);
+    }
 
     convertDate() {
         const monthNames = ["January", "February", "March",
             "April", "May", "June", "July", "August", "September",
             "October", "November", "December"];
-        let date = new Date(this.state.date_joined +'Z');
+        let date = new Date(this.state.date_joined + 'Z');
 
         return monthNames[date.getMonth() - 1] + ' ' + date.getFullYear().toString();
     }
 
+    componentDidMount() {
+      
+        fetch('http://localhost:3002/user/' + window.location.pathname.substring(1))
+            .then((res) => {
+                return res.json();
+            })
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    id: res.id,
+                    first_name: res.data.attributes.first_name,
+                    last_name: res.data.attributes.last_name,
+                    full_name: res.data.attributes.first_name + res.data.attributes.last_name,
+                    email: res.data.attributes.email,
+                    location: res.data.attributes.location,
+                    date_joined: res.data.attributes.date_joined,
+                    favorite_activities: res.data.attributes.favorite_activities,
+                    bio: res.data.attributes.bio,
+                    photo_url: res.data.attributes.photo_url,
+                    pro: res.data.attributes.pro
+                })
+            });
+    }
+
     render() {
         return (
-        <div id="profile-container">
-            {/* Possibly Add navigation topbar for other profile components */}
-            <div id="profile-subbar">
-                <h3>Profile</h3>
-            </div>
-            <div id="profile-main">
-                <div id="profile-info">
-                    <div id="profile-left-content">
-                        <div id="profile-image-container">
-                            <img id="profile-image" src={this.state.photo_url} alt={this.state.full_name}/>
+            <div id="profile-container">
+                {/* Possibly Add navigation topbar for other profile components */}
+                <div id="profile-subbar">
+                    <h3>Profile</h3>
+                </div>
+                <div id="profile-main">
+                    <div id="profile-info">
+                        <div id="profile-left-content">
+                            <div id="profile-image-container">
+                                <img id="profile-image" src={this.state.photo_url} alt={this.state.full_name} />
+                            </div>
+                            <strong>Member Since</strong>
+                            <span>{this.convertDate()}</span>
+                            {/* <button>Follow</button> Followers not implemented yet */}
                         </div>
-                        <strong>Member Since</strong>
-                        <span>{this.convertDate()}</span>
-                        {/* <button>Follow</button> Followers not implemented yet */}
-                    </div>
-                    <div id="profile-right-content">
-                        <h2 id="profile-user-name">{this.state.full_name}</h2>
-                        <Pro isPro={this.state.pro}/>
-                        <h4 id="profile-user-location">{this.state.location}</h4>
-                        <p id="profile-user-bio">{this.state.bio}</p>
-                        <h4 id="profile-favorite-activities-title">Favorite Activities</h4>
-                        <ActivitiesList activities={this.state.favorite_activities} />
+                        <div id="profile-right-content">
+                            <h2 id="profile-user-name">{this.state.full_name}</h2>
+                            <Pro isPro={this.state.pro} />
+                            <h4 id="profile-user-location">{this.state.location}</h4>
+                            <p id="profile-user-bio">{this.state.bio}</p>
+                            <h4 id="profile-favorite-activities-title">Favorite Activities</h4>
+                            <ActivitiesList activities={this.state.favorite_activities} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         )
     }
 }
--- database/config-example.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  user: 'YOUR_USERNAME_HERE', 
-
-  password: 'YOUR_PASSWORD_HERE', 
-
-  database: 'profiles_db'
-};
