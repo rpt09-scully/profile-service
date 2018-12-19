6f9c7c77a8c06cab70e512c7e6e8177dee39d070
Matt Weber <matt@weberdesign-ak.com>
2018-12-18 19:50:19 -0900

    Added detection for environment or local

--- database/index.js
+++ database/index.js
@@ -1,18 +1,27 @@
 const mysql = require('mysql');
 require('dotenv').config()
 
+const credentials = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    return {
+      database: 'profiles_db',
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+    };
+  } else {
+    return {
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT,
+      database: process.env.RDS_DB_NAME,
+    }
+  }
+}
 
-const connection = mysql.createConnection({
-  // database: 'profiles_db',
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
-});
+
+const connection = mysql.createConnection(credentials());
 
 
 const getUser = (id) => {
