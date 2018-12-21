a738e7e03ea0b6c4803d548ed4773c593fea2713
Matt Weber <matt@weberdesign-ak.com>
2018-12-20 18:17:08 -0900

    Hardcode db name

--- database/index.js
+++ database/index.js
@@ -15,7 +15,7 @@ const credentials = () => {
       user: process.env.RDS_USERNAME,
       password: process.env.RDS_PASSWORD,
       port: process.env.RDS_PORT,
-      database: process.env.RDS_DB_NAME,
+      database: 'profiles_db',
     }
   }
 }
